57df4d42937b2632d3ac953442882f10bd72e2c3
evabouba <72459357+evabouba@users.noreply.github.com>
2021-10-25 13:11:43 +0200

    Add embedding fetcher

---
+++ data/Embeddings/fetch_data.sh
@@ -0,0 +1,6 @@
+curl -Lo wiki.en.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.en.vec
+curl -Lo wiki.fr.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.fr.vec
+#curl -Lo wiki.en.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.es.vec
+#curl -Lo wiki.en.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.de.vec
+#curl -Lo wiki.en.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.he.vec
+#curl -Lo wiki.en.vec https://dl.fbaipublicfiles.com/fasttext/vectors-wiki/wiki.zh.vec
